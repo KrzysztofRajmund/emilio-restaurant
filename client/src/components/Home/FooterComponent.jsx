a22550a758ba0306a76140e6fa1d4c046fe15237
KrzysztofRajmund <knadlonek@gmail.com>
2022-09-10 21:00:53 +0200

    feat: new working hours

--- client/src/components/Home/FooterComponent.jsx
+++ client/src/components/Home/FooterComponent.jsx
@@ -75,9 +75,9 @@ const FooterComponent = () => {
                 Godziny otwarcia
                 <hr />
               </h5>
-              <p>Pn–Czw 13–21</p>
-              <p>Pt-Sb 13–22</p>
-              <p>Niedz 13–20</p>
+              <p>Pn–Pt 12–22</p>
+              <p>Sob 12–23</p>
+              <p>Niedz 12–20</p>
               <p>
                 <a href='/rezerwacja'>Rezerwuj stolik</a>
               </p>
